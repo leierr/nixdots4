168bbc8878338dd3f35996824ef074e7c8ccc1c1
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-12-27 00:54:57 +0100

    big nix-install script fix

--- iso/install.sh
+++ iso/install.sh
@@ -8,15 +8,8 @@ fi
 # test internet connectivity
 timeout 3 bash -c "</dev/tcp/archlinux.org/443" &> /dev/null
 
-flake_git_url="https://github.com/leierr/nixdotsV3.git"
-
 # print nixos logo 4 fun
 clear ; fastfetch -s ":"
-
-# get flake info & choose system to install
-gum style --border double --margin "0 1" --padding "1 2" --border-foreground "006" "Please select NixOS system to install from ${flake_git_url}"
-flake_systems="$(nix flake show --no-write-lock-file --json "git+${flake_git_url}" 2>/dev/null | jq -r '.nixosConfigurations | keys[]')"
-system_to_install="$(gum choose --cursor.foreground=002 ${flake_systems[@]})"
 
 # pretty print disks, then pick one
 clear ; gum style --border double --margin "0 1" --padding "1 2" --border-foreground "006" """Please Select disk
@@ -28,13 +21,9 @@ installdisk="$(gum choose --cursor.foreground=002 $(lsblk -dnpo NAME -I 8,259,25
 [[ -n "${installdisk}" && -b "${installdisk}" ]]
 
 clear ; gum confirm """Proceed with these settings?
-
-System: ${system_to_install}
 Disk: ${installdisk}""" --selected.background "001" --prompt.foreground="006"
 
-clear
-
-echo "Partitioning disk..."
+clear ; echo "Partitioning disk..."
 
 # Use a subshell to temporarily disable exit on error
 (
@@ -72,5 +61,61 @@ mount /dev/disk/by-label/NIXROOT /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/NIXBOOT /mnt/boot
 
-git clone ${flake_git_url} /mnt/etc/nixos/ &>/dev/null
-nixos-install --cores 0 --no-root-passwd --option eval-cache false --root /mnt --flake "/mnt/etc/nixos#${system_to_install}"
+mkdir /root/basic_ass_system
+cat <<EOF > /root/basic_ass_system/flake.nix
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  outputs = { nixpkgs, ... }@flakeInputs: {
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit flakeInputs; };
+        modules = [
+          ({ pkgs, ... }: {
+            system.stateVersion = "24.11";
+
+            # hardware + boot
+            fileSystems = {
+              "/boot" = { device = "/dev/disk/by-label/NIXBOOT"; fsType = "vfat"; };
+              "/" = { device = "/dev/disk/by-label/NIXROOT"; fsType = "ext4"; };
+            }; swapDevices = [];
+
+            boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ]; #basic shit needed to boot.
+            hardware.enableRedistributableFirmware = true;
+
+            boot.loader.systemd-boot.enable = true;
+
+            # console
+            console = { earlySetup = true; keyMap = "no"; };
+
+            #timezone
+            time.timeZone = "Europe/Oslo";
+
+            # locale
+            i18n.defaultLocale = "en_US.UTF-8";
+            i18n.extraLocaleSettings = { LC_ADDRESS = "nb_NO.UTF-8"; LC_COLLATE = "nb_NO.UTF-8"; LC_CTYPE = "en_US.UTF-8"; LC_IDENTIFICATION = "nb_NO.UTF-8"; LC_MEASUREMENT = "nb_NO.UTF-8"; LC_MESSAGES = "en_US.UTF-8"; LC_MONETARY = "nb_NO.UTF-8"; LC_NAME = "nb_NO.UTF-8"; LC_NUMERIC = "nb_NO.UTF-8"; LC_PAPER = "nb_NO.UTF-8"; LC_TELEPHONE = "nb_NO.UTF-8"; LC_TIME = "nb_NO.UTF-8"; };
+
+            # networking
+            networking.hostName = "nixos";
+            networking.networkmanager.enable = true;
+            networking.wireless.enable = false;
+
+            # packages
+            environment.systemPackages = with pkgs; [ git fastfetch coreutils ];
+
+            # nix settings.
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            nixpkgs.config.allowUnfree = true;
+
+            # making the shell a little more useable
+            programs.neovim = { enable = true; viAlias = true; vimAlias = true; defaultEditor = true; withPython3 = false; withNodeJs = false; withRuby = false; };
+            programs.starship.enable = true;
+          })
+        ];
+      };
+    };
+  };
+}
+EOF
+
+nixos-install --cores 0 --no-root-passwd --no-write-lock-file --option eval-cache false --root /mnt --flake "/root/basic_ass_system#default"
