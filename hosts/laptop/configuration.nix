6f31d419c46bf826c7dde7431a35cc5edda32440
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-12-27 01:07:47 +0100

    added system: laptop

---
+++ hosts/laptop/configuration.nix
@@ -0,0 +1,45 @@
+{ pkgs, lib, flakeInputs, ... }:
+{
+  # user account
+  system_settings.core_modules.enable = true;
+  system_settings.primary_user.username = "leier";
+  system_settings.privilege_escalation.requirePasswordForWheel = false;
+  # shell
+  system_settings.primary_user.shell = pkgs.zsh;
+  system_settings.shell_environment.enable = true;
+  system_settings.shell_environment.zsh.enable = true;
+  #
+  system_settings.graphical_environment.enable = true;
+  system_settings.graphical_environment.desktops.hyprland.enable = true;
+  system_settings.graphical_environment.desktops.gnome.enable = true;
+
+  # overwriting home-manager values
+  home_manager_modules = [
+    ({
+      programs.git.includes = [
+        {
+          condition = "hasconfig:remote.*.url:git@github.com:**/**";
+          contents = {
+            user = {
+              name = "Lars Smith Eier";
+              email = "hBm5BEqULhwPKUY@protonmail.com";
+            };
+          };
+        }
+      ];
+
+      programs.ssh = {
+        enable = true;
+        matchBlocks = {
+          "github.com" = {
+            hostname = "github.com";
+            user = "git";
+            identityFile = [ "~/.ssh/id_ed25519" ];
+          };
+        };
+      };
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [ meld obsidian fastfetch spotify brave xfce.mousepad jq ];
+}
