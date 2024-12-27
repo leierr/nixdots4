b9909224a1273b6864429f4e91e3b32b0b2c6840
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-12-27 01:41:55 +0100

    asdasdasd

--- flake.nix
+++ flake.nix
@@ -1,13 +1,36 @@
 {
   outputs = { nixpkgs, home-manager, ... }@flakeInputs:
-    let
-      mkSystem = (import ./mkSystem.nix { inherit flakeInputs; });
-    in {
-      nixosConfigurations = {
-        desktop = mkSystem { host_name = "desktop"; system_state_version = "24.11"; };
-        laptop = mkSystem { host_name = "laptop"; system_state_version = "24.11"; };
+  let
+    mkSystem = {
+      host_name, system_state_version,
+      system ? "x86_64-linux",
+      configuration ? ( ./. + "/hosts/${host_name}/configuration.nix"),
+      monitors_configuration ? ( ./. + "./hosts/${host_name}/monitors.nix" ),
+      hardware_configuration ? ( ./. + "./hosts/${host_name}/hardware_configuration.nix" ),
+    }:
+    flakeInputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit flakeInputs;
       };
+      modules = [
+        configuration
+        hardware_configuration
+        monitors_configuration
+        ./modules
+        flakeInputs.home-manager.nixosModules.home-manager
+        {
+          system.stateVersion = system_state_version;
+          networking.hostName = host_name;
+        }
+      ];
     };
+  in {
+    nixosConfigurations = {
+      desktop = mkSystem { host_name = "desktop"; system_state_version = "24.11"; };
+      laptop = mkSystem { host_name = "laptop"; system_state_version = "24.11"; };
+    };
+  };
 
   inputs = {
     # Stable and Unstable Nixpkgs
