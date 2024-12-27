eca8c288f0973ce9ca7955bfc910013a0ca5b046
Lars Smith Eier <hBm5BEqULhwPKUY@protonmail.com>
2024-12-27 02:47:50 +0100

    asdsa

--- hosts/laptop/hardware_configuration.nix
+++ hosts/laptop/hardware_configuration.nix
@@ -1,73 +1,67 @@
 { pkgs, config, ... }:
 {
-  imports = [
-    ./monitors.nix
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/NIXROOT";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/NIXBOOT";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ ];
+
+  boot.kernelModules = [
+    "acpi_call" # Useful for power management, especially for turning off the discrete GPU if needed.
   ];
 
-  config = {
-    fileSystems."/" = {
-      device = "/dev/disk/by-label/NIXROOT";
-      fsType = "ext4";
-    };
-
-    fileSystems."/boot" = {
-      device = "/dev/disk/by-label/NIXBOOT";
-      fsType = "vfat";
-    };
-
-    swapDevices = [ ];
-
-    boot.kernelModules = [
-      "acpi_call" # Useful for power management, especially for turning off the discrete GPU if needed.
+  boot.initrd = {
+    verbose = false;
+    kernelModules = [
+      "i915" # Intel graphics driver for integrated graphics (needed for boot display).
+      "iwlwifi" # Wifi drivers
+      "btusb" # I tink I need it to make bluetooth work optimally :shrug:
     ];
-
-    boot.initrd = {
-      verbose = false;
-      kernelModules = [
-        "i915" # Intel graphics driver for integrated graphics (needed for boot display).
-        "iwlwifi" # Wifi drivers
-        "btusb" # I tink I need it to make bluetooth work optimally :shrug:
-      ];
-      availableKernelModules = [
-        "xhci_pci" # USB 3.0 support.
-        "ahci" # SATA controller driver (essential for your internal SSD).
-        "usb_storage" # If you use USB drives frequently.
-        "usbhid" # USB keyboard and mouse support (typically needed for external peripherals).
-        "sd_mod" # SCSI disk support (includes SATA drives).
-        # "dm_mod" # Required for LVM/encryption.
-        # "thunderbolt" # Thunderbolt devices.
-      ];
-    };
-
-    boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
-
-    hardware = {
-      cpu.intel.updateMicrocode = true;  # Installs and applies Intel microcode updates for CPU fixes and security patches.
-      enableRedistributableFirmware = true;  # Enables firmware that isn't part of the open-source kernel, required for some hardware devices.
-
-      graphics = {
-        enable = true; # Mesa
-        enable32Bit = true;
-        extraPackages = with pkgs; [
-          intel-vaapi-driver  # Video Acceleration API (VAAPI) driver for Intel hardware to accelerate video processing.
-          intel-media-driver  # Additional media driver for Intel hardware, improving video encoding/decoding.
-        ];
-      };
-
-      trackpoint.enable = true;  # Enables support for the TrackPoint (the small red dot used as a pointing device on ThinkPads).
-      trackpoint.emulateWheel = true; # Enable scrolling while holding the middle mouse button.
-      #hardware.trackpoint.sensitivity = 200;  # Optionally adjusts the sensitivity of the TrackPoint device, if uncommented.
-    };
-
-    powerManagement.cpuFreqGovernor = "powersave";
-
-    #services.fstrim.enable = true; # SSD optimization
-    #services.fwupd.enable = true;  # Firmware updates
-
-    #services.logind.extraConfig = ''
-    #  HandleSuspendKey=suspend
-    #  HandleLidSwitch=suspend
-    #  HandleHibernateKey=hibernate
-    #'';
+    availableKernelModules = [
+      "xhci_pci" # USB 3.0 support.
+      "ahci" # SATA controller driver (essential for your internal SSD).
+      "usb_storage" # If you use USB drives frequently.
+      "usbhid" # USB keyboard and mouse support (typically needed for external peripherals).
+      "sd_mod" # SCSI disk support (includes SATA drives).
+      # "dm_mod" # Required for LVM/encryption.
+      # "thunderbolt" # Thunderbolt devices.
+    ];
   };
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+
+  hardware = {
+    cpu.intel.updateMicrocode = true;  # Installs and applies Intel microcode updates for CPU fixes and security patches.
+    enableRedistributableFirmware = true;  # Enables firmware that isn't part of the open-source kernel, required for some hardware devices.
+
+    graphics = {
+      enable = true; # Mesa
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-vaapi-driver  # Video Acceleration API (VAAPI) driver for Intel hardware to accelerate video processing.
+        intel-media-driver  # Additional media driver for Intel hardware, improving video encoding/decoding.
+      ];
+    };
+
+    trackpoint.enable = true;  # Enables support for the TrackPoint (the small red dot used as a pointing device on ThinkPads).
+    trackpoint.emulateWheel = true; # Enable scrolling while holding the middle mouse button.
+    #hardware.trackpoint.sensitivity = 200;  # Optionally adjusts the sensitivity of the TrackPoint device, if uncommented.
+  };
+
+  powerManagement.cpuFreqGovernor = "powersave";
+
+  #services.fstrim.enable = true; # SSD optimization
+  #services.fwupd.enable = true;  # Firmware updates
+
+  #services.logind.extraConfig = ''
+  #  HandleSuspendKey=suspend
+  #  HandleLidSwitch=suspend
+  #  HandleHibernateKey=hibernate
+  #'';
 }
--- mkSystem.nix
+++ mkSystem.nix
@@ -1,19 +1,18 @@
 { flakeInputs }: # so I dont have to specify it every time im creating a new system.
-{ host_name, system_state_version, system ? "x86_64-linux", extraFilesToInclude ? [] }:
+{ host_name, system_state_version, system ? "x86_64-linux" }:
 let
   nixpkgs = flakeInputs.nixpkgs;
   configuration = ./hosts/${host_name}/configuration.nix;
   hardware_configuration = ./hosts/${host_name}/hardware_configuration.nix;
+  monitors_configuration = if (builtins.pathExists ./hosts/${host_name}/monitors.nix) then ./hosts/${host_name}/monitors.nix else {};
 in
 nixpkgs.lib.nixosSystem {
   inherit system;
-  specialArgs = {
-    inherit flakeInputs;
-  };
+  specialArgs = { inherit flakeInputs; };
   modules = [
     configuration
     hardware_configuration
-    ( builtins.map (file: ./hosts/${host_name}/${file}) extraFilesToInclude )
+    monitors_configuration
     ./modules
     flakeInputs.home-manager.nixosModules.home-manager
     {
